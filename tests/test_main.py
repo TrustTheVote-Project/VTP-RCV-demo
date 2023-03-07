99155984b6784d7456b2dda0c4fd2ae19196938c
Neil Johnson <neil@cadent.com>
2023-03-07 18:06:06 -0500

    Use pytest fixture to capture VoteStoreID

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from vtp.web.api.main import app
 
 client = TestClient(app)
-_vote_store_id = ""
 
 
 def test_get_root():
@@ -12,9 +12,21 @@ def test_get_root():
     assert "version" in response.json()
 
 
+@pytest.fixture
 def test_get_vote_store_id():
     response = client.get("/vote/")
     assert response.status_code == 200
     assert "VoteStoreID" in response.json()
     # retrieve VoteStoreID from response
-    _vote_store_id = response.json()["VoteStoreID"]
+    return response.json()["VoteStoreID"]
+
+
+def test_get_empty_ballot(test_get_vote_store_id):
+    # test with invalid VoteStoreID
+    response = client.get("/vote/00000X")
+    assert response.status_code == 200
+    assert "error" in response.json()
+    # test with valid VoteStoreID
+    response = client.get(f"/vote/{test_get_vote_store_id}")
+    assert response.status_code == 200
+    assert "ballot" in response.json()
