659d46d42b55e5661c6ab0706b3da264dccef473
Sandy Currier <windoverwater@users.noreply.github.com>
2023-04-06 20:29:55 -0400

    initial stub at some json data models

---
+++ src/vtp/web/api/json_data_models.py
@@ -0,0 +1,43 @@
+"""
+Pydandic Models for the blank and cast ballots, the ballot check.  It
+is probably the case that there is not enough time to create models
+for endpoints 4 and 5 as that data is currently in more or less system
+log sytax.
+"""
+
+from typing import List, Dict
+from pydantic import BaseModel
+
+class Choice(BaseModel):
+    name: str
+    party: str = None
+    ticket_names: List[str] = None
+
+class BaseContest(BaseModel):
+    choices: List[Choice]
+    contest_type: str = None
+    max: int = None
+    selection: List[str]
+    tally: str
+    uid: str
+    win-by: float = 0.5
+
+class ContestentContest(BaseContest):
+
+class QuestionContest(BaseModel):
+    description: str
+
+class TicketContest(BaseModel):
+    contest_type: str = "ticket"
+    ticket_offices: List[str]
+
+class ContestDict(BaseModel):
+    __root__: List[Dict[str, Union[ContestentContest, QuestionContest, TicketContest]]]
+
+class CastBallot(BaseModel):
+    active_ggos: List[str]
+    ballot_filename: str
+    ballot_node: str
+    ballot_subdir: str
+    contests: Dict[str, ContestDict]
+
