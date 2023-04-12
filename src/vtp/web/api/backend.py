96b6b4a9677eacace4eb27280caab1db19d34456
Sandy Currier <windoverwater@users.noreply.github.com>
2023-04-11 20:23:31 -0400

    checkpoint during refactoring

---
+++ src/vtp/web/api/backend.py
@@ -0,0 +1,153 @@
+"""Backend support for the web-api"""
+
+import csv
+import json
+import os
+
+from vtp.ops.accept_ballot_operation import AcceptBallotOperation
+from vtp.ops.cast_ballot_operation import CastBallotOperation
+from vtp.ops.setup_vtp_demo_operation import SetupVtpDemoOperation
+from vtp.ops.verify_ballot_receipt_operation import VerifyBallotReceiptOperation
+
+
+class VtpBackend:
+    """Class to keep the namespace separate"""
+
+    ########
+    # backend demo constants
+    ########
+    # set mock mode
+    _MOCK_MODE = True
+    # where the ElectionData repo is
+    _DEFAULT_ELECTIONDATA = "VTP-mock-election.US.13"
+    # where the blank ballot is stored for the spring demo
+    # pylint: disable=line-too-long
+    _MOCK_BLANK_BALLOT = "GGOs/states/Massachusetts/GGOs/towns/Concord/blank-ballots/json/000,001,002,003,ballot.json"
+    # where the cast-ballot.json file is stored for the spring demo
+    _MOCK_CAST_BALLOT = "receipts/cast-ballot.json"
+    # where the ballot-check is stored for the spring demo
+    _MOCK_BALLOT_CHECK = "receipts/receipts.26.csv"
+    _MOCK_VOTER_INDEX = 26
+    # default guid - making one up
+    _MOCK_GUID = "01d963fd74100ee3f36428740a8efd8afd781839"
+    # backend verbosity
+    _VERBOSITY = 3
+
+    # Class variables
+    _election_data_dir = None
+    _mock_blank_ballot = None
+    _mock_cast_ballot = None
+
+    @staticmethod
+    def set_election_data():
+        """Will set _election_data_dir or raise an error"""
+        # Just return if already set
+        if not VtpBackend._election_data_dir:
+            return
+        # Start wherever here is
+        cur_dir = os.getcwd()
+        while True:
+            file_list = os.listdir(cur_dir)
+            parent_dir = os.path.dirname(cur_dir)
+            if VtpBackend._DEFAULT_ELECTIONDATA in file_list:
+                # bingo
+                VtpBackend._election_data_dir = os.path.join(
+                    cur_dir,
+                    VtpBackend._DEFAULT_ELECTIONDATA,
+                )
+                VtpBackend._mock_blank_ballot = os.path.join(
+                    VtpBackend._election_data_dir,
+                    VtpBackend._MOCK_BLANK_BALLOT,
+                )
+                VtpBackend._mock_cast_ballot = os.path.join(
+                    VtpBackend._election_data_dir,
+                    VtpBackend._MOCK_CAST_BALLOT,
+                )
+                break
+            # if dir is root dir, raise error
+            if cur_dir == parent_dir:
+                raise OSError(
+                    f"The ElecationData directory ({VtpBackend._DEFAULT_ELECTIONDATA}) "
+                    "was not found prior to reaching the root (/) of the filesystem"
+                )
+            # continue walking up
+            cur_dir = parent_dir
+
+    @staticmethod
+    def get_vote_store_id() -> str:
+        """Will return a vote_store_id, a.k.a. a guid"""
+        VtpBackend.set_election_data()
+        if VtpBackend._MOCK_MODE:
+            # in mock mode there is no guid - make one up
+            return VtpBackend._MOCK_GUID
+        operation = SetupVtpDemoOperation(
+            election_data_dir=VtpBackend._election_data_dir,
+            verbosity=VtpBackend._VERBOSITY,
+        )
+        return operation.run(guid_client_store=True)
+
+    @staticmethod
+    def get_empty_ballot(vote_store_id: str) -> dict:
+        """Given an existing guid, will return the blank ballot"""
+        VtpBackend.set_election_data()
+        if VtpBackend._MOCK_MODE:
+            # in mock mode there is no guid - make one up
+            with open(VtpBackend._MOCK_BLANK_BALLOT, "r", encoding="utf8") as infile:
+                json_doc = json.load(infile)
+            return json_doc
+        # Cet a (the) blank ballot from the backend
+        operation = CastBallotOperation(
+            guid=vote_store_id,
+            verbosity=VtpBackend._VERBOSITY,
+        )
+        return operation.run(
+            blank_ballot=VtpBackend._MOCK_BLANK_BALLOT,
+            return_bb=True,
+        )
+
+    @staticmethod
+    def cast_ballot(vote_store_id: str, cast_ballot: dict) -> dict:
+        """Will cast (upload) a cast ballot and return the ballot-check and voter-index"""
+        VtpBackend.set_election_data()
+        if VtpBackend._MOCK_MODE:
+            # Just return a mock ballot-check and voter-index
+            with open(VtpBackend._MOCK_BLANK_BALLOT, "r", encoding="utf8") as infile:
+                csv_doc = csv.reader(infile)
+            return csv_doc, VtpBackend._MOCK_VOTER_INDEX
+        # handle the incoming ballot and return the ballot-check and voter-index
+        operation = AcceptBallotOperation(
+            guid=vote_store_id,
+            verbosity=VtpBackend._VERBOSITY,
+        )
+        return operation.run(
+            cast_ballot_json=cast_ballot,
+            merge_contests=True,
+        )
+
+
+    @staticmethod
+    def verify_ballot_check(
+            vote_store_id: str,
+            ballot_check: list,
+            vote_index: int,
+        ) -> str:
+        """
+        Will verify a ballot-check and vote-inded, returning an
+        undefined string at this time.
+        """
+        VtpBackend.set_election_data()
+        if VtpBackend._MOCK_MODE:
+            # Just return a mock ballot-check and voter-index
+            with open(VtpBackend._MOCK_BLANK_BALLOT, "r", encoding="utf8") as infile:
+                csv_doc = csv.reader(infile)
+            return csv_doc, VtpBackend._MOCK_VOTER_INDEX
+        # handle the incoming ballot and return the ballot-check and voter-index
+        operation = VerifyBallotReceiptOperation(
+            guid=vote_store_id,
+            verbosity=VtpBackend._VERBOSITY,
+        )
+        return operation.run(
+            receipt_csv=ballot_check,
+            row=vote_index,
+            cvr=True,
+        )
--- src/vtp/web/api/main.py
+++ src/vtp/web/api/main.py
@@ -1,38 +1,16 @@
 """API endpoints for the VoteTrackerPlus backend"""
 
-# import json
-import os
-
 from fastapi import FastAPI
-from vtp.ops.accept_ballot_operation import AcceptBallotOperation
-from vtp.ops.cast_ballot_operation import CastBallotOperation
-from vtp.ops.setup_vtp_demo_operation import SetupVtpDemoOperation
+
+from backend import VtpBackend
 
 app = FastAPI()
-
-########
-# backend demo constants
-########
-# where the ElectionData repo is
-DEFAULT_ELECTION_DATA_DIR = (
-    "/opt/VoteTrackerPlus/demo.01/mock-clients/scanner.00/VTP-mock-election.US.13"
-)
-# pylint: disable=line-too-long
-DEFAULT_BLANK_BALLOT = "GGOs/states/Massachusetts/GGOs/towns/Concord/blank-ballots/json/000,001,002,003,ballot.json"
-# for testing only
-DEFAULT_CAST_BALLOT = os.path.join(
-    DEFAULT_ELECTION_DATA_DIR,
-    "receipts",
-    "cast-ballot.json",
-)
-# backend verbosity
-VERBOSITY = 3
 
 ########
 # local variables
 ########
 # A dict to store VoteStoreIDs
-vote_store_ids = []
+vote_store_ids = {}
 
 
 @app.get("/")
@@ -46,17 +24,8 @@ async def root() -> dict:
 async def get_vote_store_id() -> dict:
     """Create and store a unique Vote Store ID for each client"""
 
-    svdo = SetupVtpDemoOperation(
-        election_data_dir=DEFAULT_ELECTION_DATA_DIR,
-        verbosity=VERBOSITY,
-    )
-    guid = svdo.run(
-        guid_client_store=True,
-    )
-
-    vote_store_id = guid
-    # add VoteStoreID to list
-    vote_store_ids.append(vote_store_id)
+    vote_store_id = VtpBackend.get_vote_store_id()
+    vote_store_ids[vote_store_id] = "uncast"
     return {"VoteStoreID": vote_store_id}
 
 
@@ -66,38 +35,53 @@ async def get_empty_ballot(vote_store_id: str) -> dict:
     """Return an empty ballot for a given Vote Store ID"""
 
     if vote_store_id in vote_store_ids:
-        # Cet a (the) blank ballot from the backend
-        cbo = CastBallotOperation(
-            guid=vote_store_id,
-            verbosity=VERBOSITY,
-        )
-        empty_ballot = cbo.run(
-            blank_ballot=DEFAULT_BLANK_BALLOT,
-            return_bb=True,
-        )
+        empty_ballot = VtpBackend.get_empty_ballot(vote_store_id)
         return {"blank-ballot": empty_ballot}
     return {"error": "VoteStoreID not found"}
 
 
 # Endpoint #3
-@app.get("/vote/cast-ballot/{vote_store_id}")
-async def cast_ballot(vote_store_id: str, cast_ballot_json: dict = None) -> dict:
+@app.post("/vote/{vote_store_id}/cast-ballot")
+async def cast_ballot(
+        vote_store_id: str,
+        incoming_cast_ballot: dict = None,
+    ) -> dict:
     """
     Uploads the ballot; the backend accepts it, merges it, and returns
     the ballot check and voter index
     """
 
     if vote_store_id in vote_store_ids:
-        # Accept the incoming cast ballot json and return a ballot check and voter index
-        abo = AcceptBallotOperation(
-            guid=vote_store_id,
-            verbosity=VERBOSITY,
-        )
-        # ZZZ stub out cast ballot
-        ballot_check, voter_index = abo.run(
-            cast_ballot=DEFAULT_CAST_BALLOT,
-            cast_ballot_json=cast_ballot_json,
-            merge_contests=True,
-        )
-        return {"ballot_check": ballot_check, "voter_index": voter_index}
+        if vote_store_ids[vote_store_id] == "uncast":
+            ballot_check, voter_index = VtpBackend.cast_ballot(vote_store_id, incoming_cast_ballot)
+            vote_store_ids[vote_store_id] = "cast"
+            return {"ballot_check": ballot_check, "voter_index": voter_index}
+        return {"error": "This ballot has already been cast"}
     return {"error": "VoteStoreID not found"}
+
+
+# Endpoint #4
+@app.post("/vote/{vote_store_id}/verify-ballot-check")
+async def verify_ballot_check(
+        vote_store_id: str,
+        ballot_check: list = None,
+        vote_index: int = 0,
+    ) -> dict:
+    """
+    Will verify the ballot-check and voter-id.  Return values are TBD
+    pending further discussions
+    """
+
+    if vote_store_id in vote_store_ids:
+        if vote_store_ids[vote_store_id] == "cast":
+            ballot_check_doc = VtpBackend.verify_ballot_check(
+                vote_store_id,
+                ballot_check,
+                vote_index,
+            )
+            return {"ballot_check_doc": ballot_check_doc}
+        return {"error": "The supplied rest endpoint (URL) is not a valid URL"}
+    return {"error": "VoteStoreID not found"}
+
+
+# Endpoint #5
